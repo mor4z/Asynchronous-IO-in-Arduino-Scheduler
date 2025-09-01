af6573d294ebf10aab44ad254d48ec7077da3a72
mor4z <morazio.2018794@studenti.uniroma1.it>
2025-09-01 13:42:57 +0200

    rimosso free_space

--- buffer/buffer.c
+++ buffer/buffer.c
@@ -9,13 +9,12 @@ void bufferInit(RingBuffer* buffer) {
     buffer -> head = 0;
     buffer -> tail = 0;
     buffer -> size = BUFFER_SIZE;
-    buffer -> free_space = buffer -> size;
 
+    // Metto a 0 tutti i caratteri del buffer
     for (int i = 0; i < buffer -> size; i++) {
         buffer -> data[i] = 0;
     } 
     
-
     printf("[bufferInit] Buffer creato con successo\n");
 } 
 
@@ -33,23 +32,22 @@ void bufferInfo(RingBuffer* buffer) {
     printf("\n");
 
     // Stampa degli altri campi
-    printf("[buffer -> head] %u \n[buffer -> tail] %u \n[buffer -> size] %u \n[buffer -> free_space] %u\n", buffer -> head, buffer -> tail, buffer -> size, buffer -> free_space);
+    printf("[buffer -> head] %u \n[buffer -> tail] %u \n[buffer -> size] %u \n", buffer -> head, buffer -> tail, buffer -> size);
 }
 
 void bufferWrite(RingBuffer* buffer, char c) {
-    if (buffer -> free_space == 0) {
+    if ((buffer -> head + 1) % (buffer -> size) == (buffer -> tail)) {
         printf("[bufferWrite] Buffer pieno, impossibile scrivere\n");
         return;
     }
 
     buffer -> data[buffer -> head] = c;
     buffer -> head = (buffer -> head + 1) % (buffer -> size);
-    buffer -> free_space--;
     // printf("[bufferWrite] Scrittura su buffer completata con successo\n");
 }
 
 char bufferRead(RingBuffer* buffer) {
-    if (buffer -> free_space == buffer -> size) {
+    if (buffer -> head == buffer -> tail) {
         printf("[bufferRead] Buffer vuoto, impossibile leggere\n");
         return -1;
     }
@@ -57,7 +55,6 @@ char bufferRead(RingBuffer* buffer) {
     char c = buffer -> data[buffer -> tail];
     buffer -> data[buffer -> tail] = 0;
     buffer -> tail = (buffer -> tail + 1) % (buffer -> size);
-    buffer -> free_space++; 
     // printf("[bufferRead] Lettura da buffer completata con successo\n");
     return c;
 } 
--- buffer/buffer.h
+++ buffer/buffer.h
@@ -8,11 +8,10 @@ typedef struct {
     uint8_t head;               // Indice per la scrittura (ultimo carattere inserito)
     uint8_t tail;               // Indice per la lettura (primo carattere inserito)
     uint8_t size;               // Dimensione del buffer
-    uint8_t free_space;         // Potrebbe essere utile, vedremo
 } RingBuffer;
 
 
 void bufferInit(RingBuffer* buffer);                // Inizializzazione del buffer
 void bufferInfo(RingBuffer* buffer);                // Stampa i campi del buffer (per debugging)
-void bufferWrite(RingBuffer* buffer, char c);    // Scrittura di un carattere sul buffer
+void bufferWrite(RingBuffer* buffer, char c);       // Scrittura di un carattere sul buffer
 char bufferRead(RingBuffer* buffer);                // Lettura di un carattere dal buffer
